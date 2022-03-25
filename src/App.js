92006ff6bad5d1221393e54c4eb05741fa591f57
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-03-24 20:55:51 -0400

    Crando el logo y su componente en SVG ademas de la animacion de la photocard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/index'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './Styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
+import { Logo } from './components/Logo/index'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
