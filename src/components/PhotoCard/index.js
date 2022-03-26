c17d515332cee473e6585ce2f77099863f3c57a9
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-03-25 23:57:29 -0400

    Aplicando hook de lazyLoad para la lista de potoCard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,41 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_dogs.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log('yes')
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+  return (
+    <Article ref={element}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
+
+            <Button>
+              <MdFavoriteBorder size='32px' /> {likes} likes!
+            </Button>
+          </>
+      }
+    </Article>
   )
 }
